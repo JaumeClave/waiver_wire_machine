bc86e9461b0ab5085c88c1eb045068d88ca9bef9
JaumeCD <j.clavedomenech@gmail.com>
2021-02-01 22:05:41 +0100

    New Streamlit App File

--- src/visualization/streamlit_app.py
+++ src/visualization/streamlit_app.py
@@ -812,22 +812,30 @@ def _max_width_():
 # Force load in wide mode
 _max_width_()
 
-uploaded_file = st.file_uploader("Choose a file")
-def yahoo_fantasy_api_authentication():
+def st_file_selector(st_placeholder, path='.', label='Please, select a file/folder...'):
+    # get base path (directory)
+    base_path = '.' if path is None or path is '' else path
+    base_path = base_path if os.path.isdir(
+        base_path) else os.path.dirname(base_path)
+    base_path = '.' if base_path is None or base_path is '' else base_path
+    # list files in base path directory
+    files = os.listdir(base_path)
+    if base_path is not '.':
+        files.insert(0, '..')
+    files.insert(0, '.')
+    selected_file = st_placeholder.selectbox(
+        label=label, options=files, key=base_path)
+    selected_path = os.path.normpath(os.path.join(base_path, selected_file))
+    if selected_file is '.':
+        return selected_path
+    if os.path.isdir(selected_path):
+        selected_path = st_file_selector(st_placeholder=st_placeholder,
+                                         path=selected_path, label=label)
+    return selected_path
 
-    sc = OAuth2(None, None, from_file=uploaded_file)
-
-    return sc
-
-
-def yahoo_fantasy_league(sc):
-
-    gm = yfa.Game(sc, NBA)
-    league_id_list = gm.league_ids(year=SEASON)
-    league_id = "".join(str(id) for id in league_id_list)
-    league = gm.to_league(league_id)
-
-    return league
+st_session_state = get(input_path='.')
+st_session_state.input_path = st_file_selector(st_placeholder=st.empty(), path=st_session_state.input_path, label=f'Input path')
+st.text(f'> Selected \'{st_session_state.input_path}\'.')
 
 sc = yahoo_fantasy_api_authentication()
 l = yahoo_fantasy_league(sc)
